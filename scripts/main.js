de8b847baef513ab17f3ef4235e7a865b40432a2
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-02 17:26:07 +0200

    Added class Square

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
 "use strict"
+/* set sizes for canvas and change they when user resized them */
 let canvas = document.querySelector("canvas");
-
+let ctx = canvas.getContext("2d");
 function setSizes() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-
 };
 window.onresize = setSizes;
 setSizes();
+/* square-pixel */
+class Square {
+	constructor (){
+		this.width = 10;
+		this.height = 10;
+	};
+	draw(x, y){
+		ctx.fillStyle = "white";
+		ctx.fillRect(x, y, this.width, this.height);
+	}
+}
+let squarePixel = new Square;
+squarePixel.draw(100,200);
