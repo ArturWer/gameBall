a4690213bd25d9d2f635f04be032cd1dff6e31f1
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-02 15:17:33 +0200

    Added function setSizes and event window.onresize

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 "use strict"
+let canvas = document.querySelector("canvas");
+
+function setSizes() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+};
+window.onresize = setSizes;
+setSizes();
