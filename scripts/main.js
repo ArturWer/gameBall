3d8e0da988c035baf7a56117ec77e566e8206f30
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-03 09:45:15 +0200

    Realised controling the Board using a keyboard (keys A, D)

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 "use strict"
+let score = document.querySelector(".score");
+let spanScore = document.querySelector(".score span");
 /* set sizes for canvas and change they when user resized them */
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
@@ -39,6 +41,7 @@ class Board {
 		this.x = x;
 		this.width = width;
 		this.height = height;
+		this.speedX = 50;
 		this.y = canvas.height - this.height*2;
 	};
 	draw(){
@@ -46,18 +49,28 @@ class Board {
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
 }
-let ball = new Ball(600, 20);
-let board = new Board (100, 100, 200, 10);
-ball.draw();
-
-canvas.addEventListener("mousemove", function (e) {
-	console.log(e.clientX);
-	board.x = e.clientX;
-});/*
 function loop(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ball.move();
 	ball.draw();
 	board.draw();
+	window.requestAnimationFrame(loop);
 };
-window.requestAnimationFrame(loop);*/
+
+let ball = new Ball(600, 20);
+let board = new Board (100, 100, 200, 10);
+ball.draw();
+
+canvas.addEventListener("mousemove", function (e) {
+	board.x = e.clientX;
+});
+window.addEventListener("keypress", function(e){
+	e.preventDefault();
+	if ((e.code === "KeyA") && (board.x > 0)) {
+		board.x -= board.speedX;
+	} else if ((e.code === "KeyD") && (board.x + board.width < canvas.width)) {
+		board.x += board.speedX;
+	}
+	console.log(`e.key ${e.key} e.code ${e.code}`);
+});
+loop();
